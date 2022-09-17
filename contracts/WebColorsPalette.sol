6a8aed0448a7984be184e2aba9076159c76f5101
Roman Kasner <roman@kasner.pro>
2022-09-17 23:08:14 +0300

    NFT behavior implemented.

--- contracts/WebColorsPalette.sol
+++ contracts/WebColorsPalette.sol
@@ -2,18 +2,23 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract OwnableDelegateProxy {}
 
 /**
- * Used to delegate ownership of a contract to another address, 
+ * Used to delegate ownership of a contract to another address,
  * to save on unneeded transactions to approve contract use for users
  */
 contract ProxyRegistry {
     mapping(address => OwnableDelegateProxy) public proxies;
 }
 
-contract WebColorsPalette is ERC721 {
+contract WebColorsPalette is ERC721, Ownable, ReentrancyGuard {
+    using Counters for Counters.Counter;
+
     /**
      * @dev Position item on the palette
      */
@@ -24,6 +29,12 @@ contract WebColorsPalette is ERC721 {
 
     // Mapping from colors to position on the palette
     mapping(uint256 => Position) private _positions;
+
+    // version of palette
+    Counters.Counter private _version;
+
+    // Mapping from token ID to approved address to swop
+    mapping(uint256 => address) private _approvalsForChangePositions;
 
     // Proxy address for trading
     address proxyRegistryAddress;
@@ -42,6 +53,8 @@ contract WebColorsPalette is ERC721 {
         _create(creator, 0x0000FF, 0, 2);
         _create(creator, 0xFFFF00, 1, 2);
         _create(creator, 0x00FFFF, 2, 0);
+
+        _version.increment();
     }
 
     /**
@@ -56,6 +69,70 @@ contract WebColorsPalette is ERC721 {
 
         Position memory position = _positions[tokenId];
         return (position.x, position.y);
+    }
+
+    /**
+     * @dev Swap the positions of two items on the palette.
+     */
+    function changePositions(uint256 firstTokenId, uint256 secondTokenId)
+        public
+    {
+        _requireMinted(firstTokenId);
+        _requireMinted(secondTokenId);
+
+        address ownerOfFirst = ERC721.ownerOf(firstTokenId);
+        address ownerOfSecond = ERC721.ownerOf(secondTokenId);
+
+        require(
+            ownerOfFirst == _msgSender(),
+            "Change position from incorrect owner"
+        );
+        require(
+            ownerOfFirst == ownerOfSecond ||
+                getApprovedOfChangePositions(secondTokenId) == ownerOfFirst,
+            "Caller is not 'second' owner nor approved"
+        );
+
+        // Clear change position approvals from the previous mover
+        _approveOfChangePositions(address(0), secondTokenId);
+
+        Position memory temporary = _positions[secondTokenId];
+        _positions[secondTokenId] = _positions[firstTokenId];
+        _positions[firstTokenId] = temporary;
+
+        _version.increment();
+    }
+
+    /**
+     * @dev Returns the account aproved to change position of item on the palette.
+     */
+    function getApprovedOfChangePositions(uint256 tokenId)
+        public
+        view
+        returns (address)
+    {
+        _requireMinted(tokenId);
+
+        return _approvalsForChangePositions[tokenId];
+    }
+
+    /**
+     * @dev Approve change positions on palette.
+     */
+    function approveOfChangePositions(address mover, uint256 tokenId) public {
+        address owner = ERC721.ownerOf(tokenId);
+
+        require(mover != owner, "ERC721: approval to current owner");
+        require(_msgSender() == owner, "Approve caller is not token owner");
+
+        _approveOfChangePositions(mover, tokenId);
+    }
+
+    /**
+     * @dev Returns version of palette.
+     */
+    function version() public view returns (uint256) {
+        return _version.current();
     }
 
     /**
@@ -89,6 +166,13 @@ contract WebColorsPalette is ERC721 {
      */
     function _baseURI() internal view virtual override returns (string memory) {
         return "http:\\\\localhost:3000\\";
+    }
+
+    /**
+     * @dev Approve `mover` to change position of item on palette.
+     */
+    function _approveOfChangePositions(address mover, uint256 tokenId) private {
+        _approvalsForChangePositions[tokenId] = mover;
     }
 
     /**
--- test/WebColorsPalette.test.ts
+++ test/WebColorsPalette.test.ts
@@ -31,6 +31,18 @@ describe("WebColorsPalette", function () {
       expect(await wcp.balanceOf(account2.address)).to.equal(0);
     });
 
+    it("Should set the right owner", async function () {
+      const { wcp, owner } = await loadFixture(deployWebColorsPaletteFixture);
+
+      expect(await wcp.owner()).to.equal(owner.address);
+    });
+
+    it("Should returns correct version of palette", async function () {
+      const { wcp } = await loadFixture(deployWebColorsPaletteFixture);
+
+      expect(await wcp.version()).to.equal(1);
+    });
+
     it("Should returns correct name", async function () {
       const { wcp } = await loadFixture(deployWebColorsPaletteFixture);
 
@@ -140,6 +152,140 @@ describe("WebColorsPalette", function () {
         const { x, y } = await wcp.positionOf(0x000000);
         expect(x).to.eq(0);
         expect(y).to.eq(1);
+      });
+    });
+
+    describe("Change position", function () {
+      it("Should not revert bouth items owned", async function () {
+        const { wcp } = await loadFixture(deployWebColorsPaletteFixture);
+        await expect(wcp.changePositions(0xffffff, 0x000000)).not.to.be
+          .rejected;
+      });
+
+      it("Should swap positions by owner of first item", async function () {
+        const { wcp } = await loadFixture(deployWebColorsPaletteFixture);
+
+        await wcp.changePositions(0xffffff, 0x000000);
+
+        const { x: fx, y: fy } = await wcp.positionOf(0xffffff);
+        expect(fx).to.eq(0);
+        expect(fy).to.eq(1);
+      });
+
+      it("Should swop positions by owner of second item", async function () {
+        const { wcp } = await loadFixture(deployWebColorsPaletteFixture);
+
+        await wcp.changePositions(0xffffff, 0x000000);
+
+        const { x: fx, y: fy } = await wcp.positionOf(0x000000);
+        expect(fx).to.eq(0);
+        expect(fy).to.eq(0);
+      });
+
+      it("Should not swap positions by owner of second item", async function () {
+        const { wcp, account1 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await expect(wcp.connect(account1).changePositions(0xffffff, 0x000000))
+          .to.be.reverted;
+      });
+
+      it("Should change possition after transfer", async function () {
+        const { wcp, owner, account1 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await wcp.transferFrom(owner.address, account1.address, 0xffffff);
+        await wcp.transferFrom(owner.address, account1.address, 0x000000);
+
+        await expect(wcp.connect(account1).changePositions(0xffffff, 0x000000))
+          .not.to.be.reverted;
+      });
+
+      it("Should not change possition after transfer from old account", async function () {
+        const { wcp, owner, account1 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await wcp.transferFrom(owner.address, account1.address, 0xffffff);
+        await wcp.transferFrom(owner.address, account1.address, 0x000000);
+
+        await expect(wcp.changePositions(0xffffff, 0x000000)).to.be.reverted;
+      });
+    });
+
+    describe("Approve charge possition", function () {
+      it("Should approve", async function () {
+        const { wcp, account1 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await expect(wcp.approveOfChangePositions(account1.address, 0xffffff))
+          .not.to.be.reverted;
+        expect(await wcp.getApprovedOfChangePositions(0xffffff)).to.eq(
+          account1.address
+        );
+      });
+
+      it("Should not approve", async function () {
+        const { wcp, account1 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await expect(
+          wcp
+            .connect(account1)
+            .approveOfChangePositions(account1.address, 0xffffff)
+        ).to.be.reverted;
+      });
+
+      it("Should not approve to another account", async function () {
+        const { wcp, account1, account2 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        await expect(
+          wcp
+            .connect(account1)
+            .approveOfChangePositions(account2.address, 0xffffff)
+        ).to.be.reverted;
+      });
+
+      it("Should change position after approve", async function () {
+        const { wcp, owner, account1, account2 } = await loadFixture(
+          deployWebColorsPaletteFixture
+        );
+
+        const { x: x1, y: y1 } = await wcp.positionOf(0x000000);
+        expect(x1).to.eq(0);
+        expect(y1).to.eq(1);
+
+        const { x: x2, y: y2 } = await wcp.positionOf(0xffffff);
+        expect(x2).to.eq(0);
+        expect(y2).to.eq(0);
+
+        await wcp.transferFrom(owner.address, account1.address, 0xffffff);
+        await wcp.transferFrom(owner.address, account2.address, 0x000000);
+
+        await wcp
+          .connect(account1)
+          .approveOfChangePositions(account2.address, 0xffffff);
+
+        await expect(wcp.changePositions(0x000000, 0xffffff)).to.be.reverted;
+        await expect(wcp.changePositions(0xffffff, 0x000000)).to.be.reverted;
+        await expect(wcp.connect(account1).changePositions(0x000000, 0xffffff))
+          .to.be.reverted;
+
+        await wcp.connect(account2).changePositions(0x000000, 0xffffff);
+
+        const { x: x3, y: y3 } = await wcp.positionOf(0x000000);
+        expect(x3).to.eq(0);
+        expect(y3).to.eq(0);
+
+        const { x: x4, y: y4 } = await wcp.positionOf(0xffffff);
+        expect(x4).to.eq(0);
+        expect(y4).to.eq(1);
       });
     });
   });
